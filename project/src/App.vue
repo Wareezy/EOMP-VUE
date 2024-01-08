7edd49f293aeffcbface6d8a38a3e53c1f8fc576
Wareezy <warrenjaftha16@gmail.com>
2024-01-08 15:58:42 +0200

    changes to the structure

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/resume">Resume</router-link>
     <router-link to="/projects">Projects</router-link>
     <router-link to="/testimonial">Testimonial</router-link>
     <router-link to="/contact">Contact</router-link>
-  </nav>
+  </nav> -->
+  <Navbar/>
   <router-view/>
 </template>
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default{
+  components:{
+    Navbar
+  }
+}
+</script>
 
 <style>
+
+
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -22,44 +34,5 @@
 body{
   background-color: black;
 }
-  nav {
-    padding: 30px;
 
-  }
-
-  nav a {
-    /* font-weight: bold; */
-    display: inline-block;
- position: relative;
- color: white;
- font-size:20px;
- text-decoration: none;
- margin: 0 10px;
- transition: color 0.3s ease-in-out;
-  }
-
-  nav a.router-link-exact-active {
-    color: orange;
-  }
-
-  nav a:hover {
-    color: orangered; /* Change to your desired hover color */
-  }
-  nav a::after {
- content: '';
- position: absolute;
- width: 100%;
- height: 2px;
- bottom: 0;
- left: 0;
- background-color: #2c3e50;
- transform: scaleX(0);
- transform-origin: bottom right;
- transition: transform 0.25s ease-out;
-}
-
-nav a:hover::after {
- transform: scaleX(1);
- transform-origin: bottom left;
-}
 </style>
--- project/src/main.js
+++ project/src/main.js
@@ -3,5 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+const head=document.head;
+const link=document.createElement('link');
+link.rel="stylesheet";
+link.href='https://fonts.googleapis.com/css?family=Arapey';
+head.appendChild(link);
 createApp(App).use(store).use(router).mount('#app')
